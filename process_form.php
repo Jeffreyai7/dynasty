9df360ac3d6246ffae8263eb929936214a97a4e7
jeffreyai7 <jirukeh@gmail.com>
2025-01-14 15:03:39 +0100

    fix text issues

---
+++ process_form.php
@@ -0,0 +1,19 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $message = htmlspecialchars($_POST['message']);
+
+    // Email settings
+    $to = "jirukeh@gmail.com";
+    $subject = "Contact Form Submission";
+    $headers = "From: $email";
+
+    // Send email
+    if (mail($to, $subject, $message, $headers)) {
+        echo "Message sent successfully!";
+    } else {
+        echo "Error sending message.";
+    }
+}
+?>
